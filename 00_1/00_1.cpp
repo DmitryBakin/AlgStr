a639a5ba7dde0bf9f7738979b5fe62ce0876e65c
������������ <@LAPTOP-5UJ4GKLC.local>
2024-09-09 12:06:16 +0700

    Update 00_1.cpp

--- 00_1/00_1.cpp
+++ 00_1/00_1.cpp
@@ -3,10 +3,22 @@
 #include <stdlib.h>
 #include <time.h>
 #include <iostream>
+void RandMatrD(int** matr, int m, int n, int lf, int rt)
+{
+	srand(time(0));
+	for (int i = 0; i < m; i++)
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j)
+				matr[i][j] = 0;
+			else
+				matr[i][j] = rand() % (rt - lf + 1) + lf;
+		}
+}
 void InputMatrD(int** matr, int m, int n)
 {
 	for (int i = 0; i < m; i++)
-		for (int j = 0; j < n; j++) scanf_s(" % d", &matr[i][j]);
+		for (int j = 0; j < n; j++) scanf_s(" %d", &matr[i][j]);
 }
 void OutputMatrD(int** matr, int m, int n)
 {
@@ -26,6 +38,7 @@ int main()
 	matrix = new int* [Gorod];
 	for (int i = 0; i < Gorod; i++)
 		matrix[i] = new int[Gorod];
-	InputMatrD(matrix, Gorod, Gorod);
+	RandMatrD(matrix, Gorod, Gorod, 1, 100);
+	//InputMatrD(matrix, Gorod, Gorod);
 	OutputMatrD(matrix, Gorod, Gorod);
 }
