42d337f61a3fd5a2ece0212131a1fee9ecc693f4
Dmitry Bakin <dima.bakin2017@yandex.ru>
2024-10-10 11:16:40 +0700

    Update 00_1.cpp, 00_2.cpp, 00_2.sln, and 2 more files

--- 00_1/00_1.cpp
+++ 00_1/00_1.cpp
@@ -16,11 +16,6 @@ void randMatrD(int** matr, int m, int lf, int rt)
 				matr[i][j] = rand() % (rt - lf + 1) + lf;
 		}
 }
-void inputMatrD(int** matr, int m, int n)
-{
-	for (int i = 0; i < m; i++)
-		for (int j = 0; j < n; j++) scanf_s(" %d", &matr[i][j]);
-}
 void outputMatrD(int** matr, int m)
 {
 	for (int i = 0; i < m; i++)
@@ -36,12 +31,12 @@ void swap(int& a, int& b)
 	a = b;
 	b = c;
 }
-int factorial(int a)
+int inverСheck(int* massiv,int n)
 {
-	int k;
-	for (k = 1; a > 1; a--)
-		k = k * a;
-	return k;
+	for (int i = 1; i < n-1; i++)
+		if (massiv[i] < massiv[i + 1])
+			return 0;
+	return 1;
 }
 void copyMatrix(int **matrix,int **matrixCopy,int n)
 {
@@ -86,17 +81,22 @@ int priceWay(int** matr, int* massiv, int n)
 	}
 	return sum;
 }
-void dijkAlg(int** matr,int *massiv, int n)
+bool dijkAlg(int** matr,int *massiv, int n)
 {
 	int i_max = 1, j_max = 1;
-	for (int i = 2; i < n-1 ; i++)
+	for (int i = 2; i < n - 1; i++)
 		if (massiv[i] > 0 && massiv[i] < massiv[i + 1])
 			i_max = i;
-	for(int j=i_max+1;(j>i_max && j<=n-1); j++)
-		if(massiv[i_max] < massiv[j])
+	for (int j = i_max + 1; (j > i_max && j <= n - 1); j++)
+	{
+		if (massiv[i_max] < massiv[j])
 			j_max = j;
+	}
 	swap(massiv[j_max], massiv[i_max]);
 	sheiker(massiv, i_max + 1, n-1);
+	if (inverСheck(massiv,n) == 1)
+		return false;
+	return true;
 }
 void delStrStl(int** matrix,int n, int line, int column)
 {
@@ -109,22 +109,47 @@ void delStrStl(int** matrix,int n, int line, int column)
 		matrix[i][column] = 0;
 	}
 }
-int  heuristic(int **matrix,int city,int Begin)
+void exactAlg(int** matrix, int sumCities, int* massiv, int* masMax, int *masMin, int& priceMin, int& priceMax)
 {
-	int* masHeuristic;
-	int sumWayHeuristic = 1, sumHeuristic = 0;
+	priceMin = priceWay(matrix, massiv, sumCities);
+	priceMax = priceWay(matrix, massiv, sumCities);
+	for (int i = 0; i < sumCities + 1; i++)
+		masMax[i] = massiv[i];
+	for (int i = 0; i < sumCities + 1; i++)
+		masMin[i] = massiv[i];
+	do
+	{
+		int price;
+		price = priceWay(matrix, massiv, sumCities);
+		if (price > priceMax)
+		{
+			priceMax = price;
+			for (int i = 0; i < sumCities + 1; i++)
+				masMax[i] = massiv[i];
+		}
+		if (price < priceMin)
+		{
+			priceMin = price;
+			for (int i = 0; i < sumCities + 1; i++)
+				masMin[i] = massiv[i];
+		}
+	}
+	while (dijkAlg(matrix, massiv, sumCities));
+}
+void heuristic(int **matrix,int sumCities,int Begin,int *masHeuristic, int& sumHeuristic)
+{
+	int sumWayHeuristic = 1;
 	int** matrixCopy;
-	masHeuristic = new int[city + 1];
 	masHeuristic[0] = Begin;
-	masHeuristic[city] = Begin;
-	matrixCopy = new int* [city];
-	for (int i = 0; i < city; i++)
-		matrixCopy[i] = new int[city];
-	copyMatrix(matrix, matrixCopy, city);
-	while (sumWayHeuristic < city)
+	masHeuristic[sumCities] = Begin;
+	matrixCopy = new int* [sumCities];
+	for (int i = 0; i < sumCities; i++)
+		matrixCopy[i] = new int[sumCities];
+	copyMatrix(matrix, matrixCopy, sumCities);
+	while (sumWayHeuristic < sumCities)
 	{
 		int minCol = 0;
-		for (int j = 1; j <= city; j++)
+		for (int j = 1; j <= sumCities; j++)
 		{
 			if (matrix[Begin - 1][minCol] == 0)
 				minCol++;
@@ -139,21 +164,13 @@ int  heuristic(int **matrix,int city,int Begin)
 				minCol = Begin - 1;
 				sumWayHeuristic--;
 			}
-		//std::cout << matrix[Begin - 1][minCol] << "\n" << Begin << " " << minCol + 1 <<"\n" << sumWayEvr <<"\n" << "\n";
 		if (minCol + 1 != Begin)
-			delStrStl(matrix, city, Begin - 1, minCol);
-		//puts("");
-		//outputMatrD(matrix, city);
+			delStrStl(matrix, sumCities, Begin - 1, minCol);
 		matrix[minCol][Begin - 1] = 0;
 		Begin = minCol + 1;
 		sumWayHeuristic++;
 	}
-	sumHeuristic = priceWay(matrixCopy, masHeuristic, city);
-	std::cout << "\n" << "Heuristic way massiv: ";
-	for (int i = 0; i <= city; i++)
-		std::cout << masHeuristic[i] << " ";
-	delete[] masHeuristic;
-	return sumHeuristic;
+	sumHeuristic = priceWay(matrixCopy, masHeuristic, sumCities);
 }
 void percent(int priceMin, int priceMax, int price)
 {
@@ -168,88 +185,60 @@ void percent(int priceMin, int priceMax, int price)
 }
 int main()
 {
-	for (int j=0;j<2;j++)
+	for (int j=0;j<1;j++)
 	{
-		int Begin, city;
+		int Begin, sumCities;
 		std::cout << "Enter the number of cities - ";
-		std::cin >> city;
+		std::cin >> sumCities;
 		puts("");
 		std::cout << "Enter your starting city ";
 		std::cin >> Begin;
 		puts("");
 		int** matrix; int** matrixCopy;
 		int* massiv; int* masHeuristic; int* masMax; int* masMin;
-		int price = 0, priceMax = 0, priceMin = 0, sumHeuristic = 0, sumWay = 1;
+		int price = 0, priceMax = 0, sumHeuristic = 0, priceMin = 0;
 		clock_t start, stop; double time1, time2;
-		matrix = new int* [city];
-		for (int i = 0; i < city; i++)
-			matrix[i] = new int[city];
-		massiv = new int[city + 1];
-		masMax = new int[city + 1];
-		masMin = new int[city + 1];
-		randMatrD(matrix, city, 1, 100);
-		outputMatrD(matrix, city);
-		createMassivWay(city, Begin, massiv);
+		matrix = new int* [sumCities];
+		for (int i = 0; i < sumCities; i++)
+			matrix[i] = new int[sumCities];
+		massiv = new int[sumCities + 1];
+		masMax = new int[sumCities + 1];
+		masMin = new int[sumCities + 1];
+		randMatrD(matrix, sumCities, 1, 100);
+		outputMatrD(matrix, sumCities);
+		createMassivWay(sumCities, Begin, massiv);
 		start = clock();
-		priceMin = priceWay(matrix, massiv, city);
-		priceMax = priceWay(matrix, massiv, city);
-		for (int i = 0; i < city + 1; i++)
-			masMax[i] = massiv[i];
-		for (int i = 0; i < city + 1; i++)
-			masMin[i] = massiv[i];
-		/*if (city < 5)
-		{
-			for (int i = 0; i < city + 1; i++)
-				std::cout << massiv[i] << " ";
-			std::cout << "  the sum of the Way = " << priceMax << "\n";
-			}*/
-		while (sumWay != factorial((city - 1)))
-		{
-			int price;
-			dijkAlg(matrix, massiv, city);
-			price = priceWay(matrix, massiv, city);
-			/*if (city < 5)
-			{
-				for (int i = 0; i < city + 1; i++)
-					std::cout << massiv[i] << " ";
-				std::cout << "  the sum of the Way = " << price << "\n";
-			}*/
-			if (price > priceMax)
-			{
-				priceMax = price;
-				for (int i = 0; i < city + 1; i++)
-					masMax[i] = massiv[i];
-			}
-			if (price < priceMin)
-			{
-				priceMin = price;
-				for (int i = 0; i < city + 1; i++)
-					masMin[i] = massiv[i];
-			}
-				sumWay++;
-		}
+		exactAlg(matrix, sumCities, massiv, masMax, masMin, priceMin, priceMax);
 		std::cout << "\n" << "The min Way = " << priceMin << "\n" << "Minimum way: ";
-		for (int i = 0; i <= city; i++)
+		for (int i = 0; i <= sumCities; i++)
 			std::cout << masMin[i] << " ";
 		std::cout << "\n" << "The max Way = " << priceMax << "\n" << "Maximum way: ";
-		for (int i = 0; i <= city; i++)
+		for (int i = 0; i <= sumCities; i++)
 			std::cout << masMax[i] << " ";
 		puts("");
 		delete[] massiv;
+		delete[] masMax;
+		delete[] masMin;
+		masHeuristic = new int[sumCities +1];
 		stop = clock();
 		time1 = (double)(stop - start) / CLOCKS_PER_SEC;
 		std::cout << "the running time of the exact algorithm = " << time1;
 		puts("");
 		start = clock();
-		sumHeuristic = heuristic(matrix, city, Begin);
+		heuristic(matrix, sumCities, Begin, masHeuristic, sumHeuristic);
+		std::cout << "\n" << "Heuristic way massiv: ";
+		for (int i = 0; i <= sumCities; i++)
+			std::cout << masHeuristic[i] << " ";
 		stop = clock();
 		time2 = (double)(stop - start) / CLOCKS_PER_SEC;
 		std::cout << "\nThe Way in heuristic = " << sumHeuristic;
 		std::cout << "\n" << "the running time of the heuristic algorithm = " << time2;
 		percent(priceMin, priceMax, sumHeuristic);
-		for (int i = 0; i < city; i++)
+		for (int i = 0; i < sumCities; i++)
 			delete[] matrix[i];
 		delete[] matrix;
+		delete[] masHeuristic;
 		puts("\n--------------------------------------------------");
-		}
+	}
+	return 0;
 }
---
+++ 00_2/00_2.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string.h>
+#include <vector>
+
+int main()
+{
+	std::string stringHead;
+	std::cin >> stringHead;
+	std::vector<char> string;
+	std::cout << std::size(stringHead) << std::endl;
+	int i=0;
+	while(stringHead[i] != "/0")
+	{
+		string.push_back(stringHead[i]);
+		std::cout << string[i] << std::endl;
+		i++;
+	}
+}
