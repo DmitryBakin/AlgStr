5e508f3ba36afc096e8d1b1e6d954c7b5f0c49dc
Dmitry Bakin <dima.bakin2017@yandex.ru>
2024-10-14 10:58:47 +0700

    Update 00_2.cpp

--- 00_2/00_2.cpp
+++ 00_2/00_2.cpp
@@ -4,15 +4,5 @@
 
 int main()
 {
-	std::string stringHead;
-	std::cin >> stringHead;
-	std::vector<char> string;
-	std::cout << std::size(stringHead) << std::endl;
-	int i=0;
-	while(stringHead[i] != "/0")
-	{
-		string.push_back(stringHead[i]);
-		std::cout << string[i] << std::endl;
-		i++;
-	}
+	
 }
