f1b9f4aab50e9171abe79312f992c4d9dfffe76c
Dmitry Bakin <dima.bakin2017@yandex.ru>
2024-11-07 13:40:56 +0700

    Update

--- 00_3/00_3.cpp
+++ 00_3/00_3.cpp
@@ -3,6 +3,9 @@
 #include <cmath>
 #include <random>
 #include <time.h>
+#include <stdio.h>
+#include <fstream>
+#include <string>
 
 bool checkArray(int* array,int const sum)
 {
@@ -35,9 +38,6 @@ void createStepsArr2(std::vector<int>& stepsArr, int const sum)
 	{
 		stepsArr.push_back(pow(2, i) - 1);
 	}
-	for (int i = 0; i < stepsArr.size(); i++)
-		std::cout << stepsArr[i] << " ";
-	puts("");
 }
 void createStepsArr3(std::vector<int>& stepsArr, int const sum)
 {
@@ -48,64 +48,92 @@ void createStepsArr3(std::vector<int>& stepsArr, int const sum)
 		stepsArr.insert(stepsArr.begin(), 3 * stepsArr[0] + 1);
 	}
 }
-void insert(int *array,int const sum, int const b,int const s)
+void insert(int *array,int const sum, int const firstElementIndex,int const step)
 {
-	int j = b + s, x, k;
+	int j = firstElementIndex + step, x, k;
 	while (j < sum)
 	{
 		x = array[j];
-		k = j - s;
+		k = j - step;
 		while (k >= 0 && array[k] > x)
 		{
-			array[k+s] = array[k];
-			k = k - s;
+			array[k+step] = array[k];
+			k -= step;
 		}
-		array[k+s] = x;
-		j += s;
+		array[k+step] = x;
+		j += step;
 	}
 }
 void shell(int* array, std::vector<int>& stepsArr, int const sum)
 {
-	int round = 0, s, b;
+	int round = 0, step, firstElementIndex;
 	while (round <= stepsArr.size() - 1)
 	{
-		s = stepsArr[round];
-		b = 0;
-		while (b < s)
+		step = stepsArr[round];
+		firstElementIndex = 0;
+		while (firstElementIndex < step)
 		{
-			insert(array, sum, b, s);
-			b++;
+			insert(array, sum, firstElementIndex, step);
+			firstElementIndex++;
 		}
 		round++;
 	}
 }
 void main()
 {
-	for (int i = 0; i < 1; i++)
+	std::ofstream* files = new std::ofstream[9];
+	for (int i = 0, border = 10; border <= 100000; border *= 100)
 	{
-		int sum, lf, rf;
-		clock_t start, stop;
-		double time1, time2;
-		std::cout << "Enter the number of elements in the array ";
-		std::cin >> sum;
-		std::cout << "Enter lf ";
-		std::cin >> lf;
-		std::cout << "Enter rf ";
-		std::cin >> rf;
-		std::vector<int> stepsArr;
-		int* array;
-		array = new int[sum];
-		randMasD(array, sum, lf, rf);
-		for (int i = 0; i < sum; i++)
-			std::cout << array[i] << " ";
-		puts("\n------------------------------ ");
-		start = clock();
-		createStepsArr1(stepsArr, sum);
-		shell(array, stepsArr, sum);
-		stop = clock();
-		time1 = (double)(stop - start) / CLOCKS_PER_SEC;
-		std::cout << "Running time of the first algorithm = " << time1 << std::endl;
-		if (checkArray(array, sum))
-			std::cout << "\nThe array has been sorted\n";
+		for (int sum = 10000; sum <= 1000000; sum *= 10,i++)
+		{
+			clock_t start, stop;
+			double time1, time2;
+			std::vector<int> stepsArr;
+			int* array;
+			int* arrayCopy;
+			array = new int[sum];
+			arrayCopy = new int[sum];
+			randMasD(array, sum, -border, border);
+			for (int k = 0; k < sum; k++)
+				arrayCopy[k] = array[k];
+			files[i].open("random_mas_" + std::to_string(sum) + "_" + std::to_string(border) + ".txt");
+			for (int g = 0; g < sum; g++)
+				files[i] << array[g] << " ";
+			puts("\n------------------------------ ");
+			start = clock();
+			createStepsArr1(stepsArr, sum);
+			shell(array, stepsArr, sum);
+			stop = clock();
+			time1 = (double)(stop - start) / CLOCKS_PER_SEC;
+			std::cout << "Running time of the first algorithm(border = "<< border << ", size = " << sum << ") = " << time1 << std::endl;
+			if (checkArray(array, sum))
+				std::cout << "\nThe array has been sorted\n";
+			puts("\n------------------------------ ");
+			for (int k = 0; k < sum; k++)
+				array[k] = arrayCopy[k];
+			stepsArr.clear();
+			start = clock();
+			createStepsArr2(stepsArr, sum);
+			shell(array, stepsArr, sum);
+			stop = clock();
+			time1 = (double)(stop - start) / CLOCKS_PER_SEC;
+			std::cout << "Running time of the second algorithm(border = " << border << ", size = " << sum << ") = " << time1 << std::endl;
+			if (checkArray(array, sum))
+				std::cout << "\nThe array has been sorted\n";
+			puts("\n------------------------------ ");
+			stepsArr.clear();
+			for (int k = 0; k < sum; k++)
+				array[k] = arrayCopy[k];
+			start = clock();
+			createStepsArr3(stepsArr, sum);
+			shell(array, stepsArr, sum);
+			stop = clock();
+			time1 = (double)(stop - start) / CLOCKS_PER_SEC;
+			std::cout << "Running time of the third algorithm(border = " << border << ", size = " << sum << ") = " << time1 << std::endl;
+			if (checkArray(array, sum))
+				std::cout << "\nThe array has been sorted\n";
+			delete[]array;
+		}
 	}
+	delete[]files;
 }
